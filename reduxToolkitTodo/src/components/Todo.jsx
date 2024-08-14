67290be4638630eb7ae4d8e49567616e0ed6855c
Desktop Taqi <shaikmohammedtaqidanish88@gmail.com>
2024-08-15 01:26:47 +0530

    redux

--- reduxToolkitTodo/src/components/Todo.jsx
+++ reduxToolkitTodo/src/components/Todo.jsx
@@ -3,6 +3,7 @@ import { useSelector,useDispatch   } from 'react-redux'
 import { removeTodo } from '../features/todo/todoSlice'
 function Todo() {
   const todos= useSelector((state) => state.todos)
+  const dispatch = useDispatch()
   return (
     <>
     <div>Todos</div>
